f2fc7e087998e9579a37a4d2a70d85f0640acc70
Roopali Vij <roopali.vij@globallogic.com>
2015-12-17 17:03:01 +0530

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularAutoDeployApp
+ * @description
+ * # angularAutoDeployApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularAutoDeployApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAutoDeployApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularAutoDeployApp
+ */
+angular.module('angularAutoDeployApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
